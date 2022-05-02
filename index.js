70f4ade641f484abb16e734aa2f90228c59b8b18
Eitan Mizrachi Belisario <eitanmb@gmail.com>
2022-05-02 11:50:03 -0400

    apollo configured

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({ typeDefs, resolvers });
   const { url } = await server.listen();
   console.log(`🚀 Server ready at ${url}`);
 }
+
+
+
+startApolloServer(typeDefs, resolvers);
