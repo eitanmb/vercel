2d47a33fb12bf9d6c5a1507446fe96a9f0ca09af
Eitan Mizrachi Belisario <eitanmb@gmail.com>
2022-05-02 11:45:35 -0400

    index added

--- dev.js
+++
@@ -1,8 +0,0 @@
-import {app} from "index";
-import consola from "consola";
-import dotenv from "dotenv";
-dotenv.config();
-
-
-console.log("dev");
-app.listen(3000, () => consola.info("Server started"));
